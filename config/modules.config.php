2c5c0e186c2cadde1c83be68ef210d20c58d0c86
Wiktoria Hoffmann <whoffmann2000@wp.pl>
2023-01-16 11:03:41 +0100

    Created Laminas Skeleton

---
+++ config/modules.config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\Form',
+    'Laminas\I18n',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Hydrator',
+    'Laminas\Db',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Application',
+];
